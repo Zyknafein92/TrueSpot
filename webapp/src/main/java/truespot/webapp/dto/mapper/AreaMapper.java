aa582847881d215d905db3cd903e15062a5d708f
Zyk <deneux.j@gmail.com>
2019-04-11 20:14:01 +0200

    Add Mapper package and class

---
+++ webapp/src/main/java/truespot/webapp/dto/mapper/AreaMapper.java
@@ -0,0 +1,4 @@
+package truespot.webapp.dto.mapper;
+
+public class AreaMapper {
+}
---
+++ webapp/src/main/java/truespot/webapp/dto/mapper/UserMapper.java
@@ -0,0 +1,44 @@
+package truespot.webapp.dto.mapper;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import truespot.business.contract.UserManager;
+import truespot.model.User;
+import truespot.webapp.dto.UserDTO;
+
+@Component
+public class UserMapper {
+
+    @Autowired
+    private UserManager userManager;
+
+     public UserDTO objectToDTO (User user) {
+
+         UserDTO userDTO = new UserDTO();
+
+         userDTO.setPseudo(user.getPseudo() != null ? user.getPseudo() : "");
+         //todo tous les attributs
+
+
+         return userDTO;
+     }
+
+     public User dtoToObject (UserDTO userDTO){
+         User user = new User();
+         //todo :  même chose inverse
+
+         return user;
+     }
+
+     public User updateRecord(Long id, User uUpdate){
+
+         User user = new User();
+
+         user = userManager.getUser(id);
+         //todo.. setter ect...
+
+         return user;
+     }
+
+}
