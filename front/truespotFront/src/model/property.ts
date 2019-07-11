c1f6645e009e422db946342408736ab8db70a87f
Zyk <deneux.j@gmail.com>
2019-07-11 17:43:17 +0200

    Try Init JWT Angular / Add signIn/Logout component v.2

--- front/truespotFront/src/model/property.ts
+++ front/truespotFront/src/model/property.ts
@@ -13,14 +13,14 @@ import { Xml } from './xml';
 
 
 export interface Property {
-    position?: number;
-    title?: string;
-    vendorExtensions?: { [key: string]: any; };
-    xml?: Xml;
-    example?: any;
-    allowEmptyValue?: boolean;
     readOnly?: boolean;
     type?: string;
-    description?: string;
     format?: string;
+    position?: number;
+    description?: string;
+    title?: string;
+    vendorExtensions?: { [key: string]: any; };
+    allowEmptyValue?: boolean;
+    example?: any;
+    xml?: Xml;
 }
